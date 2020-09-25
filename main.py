448b3cf33998260d6353e8f221ea32f204b4e934
Word90Ninety <jktan1160@gmail.com>
2020-09-25 21:13:48 +0000

    minor

--- main.py
+++ main.py
@@ -2,16 +2,24 @@ print(__name__)
 
 from group import Group
 from cayley_tabler import cayley_tabler, xor
-from permutator import even_permuter, permutation_composer
+from permutator import permuter, even_permuter, permutation_composer
+from group_composer import direct_group_product
 
-topic = "cayley"
+topic = "perms"
 
 if __name__ == "__main__":
   if topic == "cayley":
     table = cayley_tabler(Group([1, 2], xor), True, True)
     for row in table:
       print(row)
+  elif topic == "product":
+    direct_group_product(1, 1)
   elif topic == "perms":
-    evens = cayley_tabler(Group(even_permuter(4), permutation_composer))
+    perms = cayley_tabler(Group(permuter(4), permutation_composer), True, True)
+    for line in perms:
+        print(line)
+  elif topic == "eperms":
+    evens = cayley_tabler(Group(even_permuter(4), permutation_composer), True, True)
     for line in evens:
         print(line)
+    
